11b152dee18ecbb859fb789916ad5da580ae40e4
TrennyUK <trennyuk@gmail.com>
2025-07-13 11:54:19 +0700

    Update Dash_Board_Chart.js

--- Dash_Board_Chart.js
+++ Dash_Board_Chart.js
@@ -12,7 +12,7 @@ const RANGE_NAMES = 'Day 1!C5:C49';
 // 📌 sheetName: "Month 1", "Month 2", v.v...
 // 📌 Lấy giá trị từ ô AM35 và hiển thị vào .salary
 function fetchSalaryFromSheet(sheetId, sheetName) {
-    const CELL = 'AN35';
+    const CELL = 'AN50';
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${encodeURIComponent(sheetName)}!${CELL}?key=${API_KEY}`;
 
     fetch(url)
